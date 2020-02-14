2a886f50e35eba674ac70b0c09353f959d8aa040
Shreekar <sshreeka@redhat.com>
2020-02-14 12:07:42 +0530

    tests/manage/z_cluster/cluster_expansion/test_cluster_fill.py
    Signed-off-by: Shreekar <sshreeka@redhat.com>

--- tests/manage/z_cluster/cluster_expansion/test_cluster_fill.py
+++ tests/manage/z_cluster/cluster_expansion/test_cluster_fill.py
@@ -3,6 +3,7 @@ import pytest
 from concurrent.futures import ThreadPoolExecutor
 
 from ocs_ci.framework.testlib import tier4, ignore_leftovers, ManageTest
+from ocs_ci.ocs import constants
 from tests.manage.z_cluster.cluster_expansion.create_delete_pvc_parallel import test_create_delete_pvcs
 from tests.manage.mcg.helpers import s3_io
 from tests.manage.z_cluster import tier4_helpers
@@ -20,23 +21,16 @@ class TestAddNode(ManageTest):
     def test_run_io_multiple_dc_pods(self,project_factory,multi_dc_pod,multi_pvc_factory, pod_factory,mcg_obj, awscli_pod, bucket_factory):
         project = project_factory()
         pods = multi_dc_pod(num_of_pvcs=10,pvc_size=200,project=project)
-        rbd_rawblock_pods =  pods[int(len(pods)/2)::]
-        non_rawblock_pods = (list(set(pods) - set(rbd_rawblock_pods)))
-
-
-        logger.info(len(rbd_rawblock_pods))
-        logger.info(len(non_rawblock_pods))
 
         executor = ThreadPoolExecutor(max_workers=32)
-        for non_rawblock_pod in non_rawblock_pods:
-            executor.submit(tier4_helpers.cluster_fillup, pod=non_rawblock_pod)
-        for rawblockpod in rbd_rawblock_pods:
-            executor.submit(tier4_helpers.raw_block_io,pod=rawblockpod)
-
+        for pod in pods:
+            if pod.pvc.get_pvc_access_mode == constants.ACCESS_MODE_RWX:
+                executor.submit(tier4_helpers.raw_block_io, pod=pod)
+            else:
+                executor.submit(tier4_helpers.cluster_fillup,pod=pod)
 
         executor.submit(test_create_delete_pvcs,multi_pvc_factory, pod_factory, project)
         executor.submit(s3_io, mcg_obj, awscli_pod, bucket_factory)
         assert tier4_helpers.check_cluster_size(50)
         logging.info('Pass')
-
         #Todo - integrate add node and add capacity UI automation
