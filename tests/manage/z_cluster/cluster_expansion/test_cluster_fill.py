b2935229b41d8723528955deba520e316933d7f5
Shreekar <sshreeka@redhat.com>
2020-02-14 20:20:46 +0530

    tests/manage/z_cluster/cluster_expansion/test_cluster_fill.py
    Signed-off-by: Shreekar <sshreeka@redhat.com>

--- tests/manage/z_cluster/cluster_expansion/test_cluster_fill.py
+++ tests/manage/z_cluster/cluster_expansion/test_cluster_fill.py
@@ -10,6 +10,7 @@ from tests.manage.z_cluster import tier4_helpers
 
 from ocs_ci.framework.pytest_customization.marks import (filter_insecure_request_warning,
 )
+from ocs_ci.framework import config
 
 logger = logging.getLogger(__name__)
 
@@ -19,18 +20,23 @@ logger = logging.getLogger(__name__)
 @tier4
 class TestAddNode(ManageTest):
     def test_run_io_multiple_dc_pods(self,project_factory,multi_dc_pod,multi_pvc_factory, pod_factory,mcg_obj, awscli_pod, bucket_factory):
+        osd_size = config.ENV_DATA.get('osd','2T')
+        pvc_num = config.ENV_DATA.get('pvc_num',10)
+        pvc_size = config.ENV_DATA.get('pvc_size',200)
+        #
         project = project_factory()
-        pods = multi_dc_pod(num_of_pvcs=10,pvc_size=200,project=project)
+        pods = multi_dc_pod(num_of_pvcs=pvc_num,pvc_size=pvc_size,project=project)
 
         executor = ThreadPoolExecutor(max_workers=32)
         for pod in pods:
             if pod.pvc.get_pvc_access_mode == constants.ACCESS_MODE_RWX:
-                executor.submit(tier4_helpers.raw_block_io, pod=pod)
+                if 'rbd' in pod.name:
+                    executor.submit(tier4_helpers.raw_block_io, pod=pod)
             else:
-                executor.submit(tier4_helpers.cluster_fillup,pod=pod)
+                executor.submit(tier4_helpers.cluster_fillup,pod=pod,osd_size=osd_size)
 
         executor.submit(test_create_delete_pvcs,multi_pvc_factory, pod_factory, project)
         executor.submit(s3_io, mcg_obj, awscli_pod, bucket_factory)
         assert tier4_helpers.check_cluster_size(50)
         logging.info('Pass')
-        #Todo - integrate add node and add capacity UI automation
+        # #Todo - integrate add node and add capacity UI automation
