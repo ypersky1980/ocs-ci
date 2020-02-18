56624f749940d92558ee999ca78520ca37fa7755
Shreekar <sshreeka@redhat.com>
2020-02-18 11:48:26 +0530

    tests/manage/z_cluster/tier4_helpers.py
    Signed-off-by: Shreekar <sshreeka@redhat.com>

--- tests/manage/z_cluster/tier4_helpers.py
+++ tests/manage/z_cluster/tier4_helpers.py
@@ -6,28 +6,37 @@ import ocs_ci.ocs.resources.pod as pod
 
 logger = logging.getLogger(__name__)
 
-def raw_block_io(pod,size='100G'):
+def raw_block_io(pod,osd_size='2T'):
+    if osd_size == '2T':
+        size = '100G'
+    elif osd_size == '4T':
+        size = '200G'
+    else:
+        size = '50G'
     pod.run_io(storage_type = 'block', size = size)
     return True
 
 
-def cluster_fillup(pod):
-    """
-    Todo
-    Make IO sizes configurable
-    """
-    logger.info('filling up..')
-    cmd1 = """
-mkdir /mnt/temp{1..30} ;curl http://download.ceph.com/tarballs/ceph_14.2.4.orig.tar.gz --output /mnt/temp1/ceph.tar.gz;
-    """
-    cmd2 = """
-    for i in {1..30}; do cp /mnt/temp1/ceph.tar.gz /mnt/temp1/ceph_$i & done
-"""
-    cmd3 = """
-for i in {2..30};
+def cluster_fillup(pod,osd_size='2T'):
+    logger.info(f'filling up on pod {pod.name}')
+    if osd_size == '2T':
+        dir_num = 30
+    elif osd_size == '4T':
+        dir_num = 60
+    else:
+        dir_num = 15
+    cmd1 = f'''
+mkdir /mnt/temp{{1..{dir_num}}} ;
+curl http://download.ceph.com/tarballs/ceph_14.2.4.orig.tar.gz --output /mnt/temp1/ceph.tar.gz;
+'''
+    cmd2 = f'''
+for i in {{1..{dir_num}}}; do cp /mnt/temp1/ceph.tar.gz /mnt/temp1/ceph_$i & done
+'''
+    cmd3 = f'''
+for i in {{2..{dir_num}}};
 do cp -r /mnt/temp1/* /mnt/temp$i/. &
 done
-    """
+'''
     pod.exec_bash_cmd_on_pod(command=cmd1)
     pod.exec_bash_cmd_on_pod(command=cmd2)
     pod.exec_bash_cmd_on_pod(command=cmd3)
@@ -46,7 +55,8 @@ def check_cluster_size(size):
             logger.info('rechecking.......')
             retries += 1
             pass
-        if retries > 20 and ceph_obj.get_used_space(ct_pod) <= 5:
+        if retries > 5 and ceph_obj.get_used_space(ct_pod) <= 5:
             logger.error('IOs not happening ')
             return False
         time.sleep(1200)
+
