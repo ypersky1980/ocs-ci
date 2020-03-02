b5be92a9a6fc81ceb14d82274d2b551eafd2dab1
Harish NV Rao <hnallurv@redhat.com>
2020-03-02 14:09:30 +0530

    modified:   tests/manage/z_cluster/tier4_helpers.py
    Signed-off-by: Harish NV Rao <hnallurv@redhat.com>

--- tests/manage/z_cluster/tier4_helpers.py
+++ tests/manage/z_cluster/tier4_helpers.py
@@ -1,16 +1,17 @@
 import logging
 import time
-from ocs_ci.ocs.cluster import CephCluster, count_cluster_osd
-# from ocs_ci.ocs.resources import pod
-import ocs_ci.ocs.resources.pod as pod
 from ocs_ci.ocs import constants
 from ocs_ci.ocs import defaults
 from ocs_ci.ocs.ocp import OCP
+import ocs_ci.ocs.resources.pod as pod_helpers
+from ocs_ci.ocs.cluster import CephCluster, count_cluster_osd
+
+from uuid import uuid4
 
 logger = logging.getLogger(__name__)
 
-def raw_block_io(pod,size='100G'):
-    pod.run_io(storage_type = 'block', size = size)
+def raw_block_io(raw_blk_pod,size='100G'):
+    raw_blk_pod.run_io(storage_type = 'block', size = size)
     return True
 
 
@@ -25,96 +26,67 @@ def get_osd_size():
         get('spec').get('resources').get('requests').get('storage')[:-2]
 
 
-def cluster_fill_get_pod_iter(percent_capacity_to_fill=75):
+def cluster_fillup(fill_pod, percent_required_filled):
 
-    # this dictionary is based on the:
-    # https://docs.google.com/spreadsheets/d/1XXqDNB6ujiQg--qyoJvB18GPgv_sIi8Xt4-c2tYtD1o/edit#gid=0&range=B15
+    logging.info(f"#### Filling up cluster FROM Pod: {fill_pod.name}")
 
-    dict_pod_iter = {
-        "25":
-            {
-                "512": {"pod": 3, "iteration": 1},
-                "2": {"pod": 6, "iteration": 2},
-                "4": {"pod": 24, "iteration": 1}
-            },
-        "50":
-            {
-                "512": {"pod": 6, "iteration": 1},
-                "2": {"pod": 12, "iteration": 2},
-                "4": {"pod": 24, "iteration": 2}
-            },
-        "75":
-            {
-                "340": {"pod": 6, "iteration": 1},
-                "512": {"pod": 9, "iteration": 1},
-                "2": {"pod": 18, "iteration": 2},
-                "4": {"pod": 36, "iteration": 2}
-            },
-        "85":
-            {
-                "340": {"pod": 6, "iteration": 1},
-                "512": {"pod": 9, "iteration": 1},
-                "2": {"pod": 21, "iteration": 2},
-                "4": {"pod": 42, "iteration": 2}
-            },
-        "37": # Testing purpose on ocs-ci based cluster. Not for production
-            {
-                "340": {"pod": 3, "iteration": 1}
-            }
-
-    }
-    osd_size = get_osd_size()
-    pods = dict_pod_iter[percent_capacity_to_fill][osd_size]["pod"]
-    iterations = dict_pod_iter[percent_capacity_to_fill][osd_size]["iteration"]
-    logging.info(f"pods to create = {pods}. iterations to run = {iterations}")
-    return pods, iterations
-
-
-def IOs(percent_to_fillup):
-    num_of_osds = count_cluster_osd()
-    osd_size = get_osd_size()
-    total_capacity = num_of_osds * osd_size
-    capacity_to_fillup = total_capacity * percent_to_fillup/100
-
-
-def cluster_fillup(pod, iterations=1):
-    logging.info(f"#### Filling up cluster FROM Pod: {pod.name}")
-
-    cmd1 = """
-            mkdir /mnt/temp{1..30} ;
-            curl http://download.ceph.com/tarballs/ceph_15.1.0.orig.tar.gz --output ceph.tar.gz;
-        """
-    pod.exec_sh_cmd_on_pod(cmd1,  sh="bash", timeout=None)
+    cmd1 = """curl http://download.ceph.com/tarballs/ceph_15.1.0.orig.tar.gz --output /tmp/ceph.tar.gz """
+    logging.info(f"#### Command 1 is: {cmd1}")
+    fill_pod.exec_sh_cmd_on_pod(cmd1, sh="bash")
     logging.info("#### executed cmd1")
 
-    cmd2 = """
-                for i in {1..10}; do cp ceph.tar.gz /mnt/temp1/ceph_$i.tar.gz & done
-            """
-    pod.exec_sh_cmd_on_pod(cmd2,  sh="bash", timeout=None)
+    dir_name = "cluster_fillup_" + uuid4().hex
+
+    cmd2 = "" + "mkdir /mnt/" + dir_name + ";" + "mkdir /mnt/" + dir_name + "/temp1;" + \
+           "for i in {1..30}; do cp /tmp/ceph.tar.gz /mnt/" + dir_name +"/temp1/ceph_$i.tar.gz & done" + ""
+    logging.info(f"#### Command 2 is: {cmd2}")
+    fill_pod.exec_sh_cmd_on_pod(cmd2,  sh="bash")
     logging.info("#### executed cmd2")
 
-    cmd3 = """
-                for i in {2..30};
-                do 
-                        cp -r /mnt/temp1/* /mnt/temp$i/. &
-                    done
-        """
+    ct_pod = pod_helpers.get_ceph_tools_pod()
+    src_dir = "/mnt/" + dir_name + "/temp1/"
+    dir_num = 2
+    percent_used_capacity = 0
+    cluster_filled = False
 
-    for i in range(int(iterations)):
-        pod.exec_sh_cmd_on_pod(cmd3,  sh="bash", timeout=None)
+    while not cluster_filled:
+        dest_dir = "/mnt/"+dir_name+"/temp"+str(dir_num)
+        cmd = "" + "mkdir " + dest_dir + ""
+        fill_pod.exec_sh_cmd_on_pod(cmd, sh="bash")
+        for i in range(1, 30):
+            cmd = "" + "cp " + src_dir + "/ceph_" + str(i) + ".tar.gz  " + dest_dir + "/. ;" + ""
+            logging.info(f"#### Command 3 is: {cmd} ####")
+            fill_pod.exec_sh_cmd_on_pod(cmd, sh="bash")
+            logging.info("#### executed cmd3 ####")
+            # use a function from cluster.py to replace these following 2 lines
+            output = ct_pod.exec_ceph_cmd(ceph_cmd='rados df')
+            percent_used_capacity = (output.get('total_used') / output.get('total_space')) * 100
+            logging.info(f"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ percent_used_capacity = {percent_used_capacity}. "
+                         f"percent required to fill = {percent_required_filled} ####")
+            if percent_used_capacity >= percent_required_filled:
+                logging.info(f"##### Inside if. used = {percent_used_capacity}, required = {percent_required_filled}")
+                cluster_filled = True
+                break # break the for loop
+        dir_num += 1
+    """
+    while percent_used_capacity < percent_required_filled:
 
-    logging.info("#### executed cmd3")
-    ceph_obj = CephCluster()
-    from ocs_ci.ocs.resources import pod
-    ct_pod = pod.get_ceph_tools_pod()
-    output = ct_pod.exec_ceph_cmd(ceph_cmd='rados df')
-    used_capacity = output.get('total_used')
-    logging.info(f"***** Used space = {used_capacity} GiB")
+        cmd3 = ""+ "mkdir /mnt/"+dir_name+"/temp"+str(dir_num)+";"\
+               "cp -r /mnt/"+ dir_name + "/temp1/."+ " /mnt/"+dir_name+"/temp"+str(dir_num)+"/. ;" + ""
+        logging.info(f"#### Command 3 is: {cmd3}")
+        fill_pod.exec_sh_cmd_on_pod(cmd3,  sh="bash")
+        logging.info("#### executed cmd3")
+        output = ct_pod.exec_ceph_cmd(ceph_cmd='rados df')
+        percent_used_capacity = (output.get('total_used')/output.get('total_space'))*100
+        dir_num += 1
+    """
+    logging.info(f"Completed cluster fill from pod {fill_pod.name}")
+    logging.info(f"Percent filled is {percent_used_capacity}")
 
 
 def check_cluster_size(size):
     ceph_obj = CephCluster()
-    ct_pod = pod.get_ceph_tools_pod()
+    ct_pod = pod_helpers.get_ceph_tools_pod()
     retries = 0
     while True:
         if ceph_obj.get_used_space(ct_pod) >= size:
@@ -129,51 +101,51 @@ def check_cluster_size(size):
             return False
         time.sleep(1200)
 
-def cluster_copy_ops(pod, iterations=10):
+def cluster_copy_ops(copy_pod, iterations=10):
+
+    dir_name = "cluster_copy_ops_" + uuid4().hex
 
     for itr in range(iterations):
-        # wget ceph.tar.gz
-        cmd = """
-                curl http://download.ceph.com/tarballs/ceph_15.1.0.orig.tar.gz --output ceph.tar.gz;
-            """
-        pod.exec_sh_cmd_on_pod(cmd, sh="bash", timeout=None)
-
         # cp ceph.tar.gz 10 times to each cpdir
-        cmd = """
-                mkdir /mnt/copy_dir{1..10} ;
-                for i in {1..10}; do cp ceph.tar.gz /mnt/copy_dir$i/ceph_$i.tar.gz & done
-                """
-        pod.exec_sh_cmd_on_pod(cmd, sh="bash", timeout=None)
+        cmd = "" + "mkdir /mnt/"+ dir_name + "; mkdir /mnt/"+ dir_name + "/copy_dir{1..10} ; " \
+                   "for i in {1..10}; do cp /tmp/ceph.tar.gz /mnt/"+ dir_name + "/copy_dir$i/ceph_$i.tar.gz & done"\
+              + ""
+        copy_pod.exec_sh_cmd_on_pod(cmd, sh="bash")
 
         # check md5sum
         # since the file to be copied is from a wellknown location, we calculated its md5sum and found it to be:
         # 016c37aa72f12e88127239467ff4962b. We will pass this value to the pod to see if it matches that of the same
         # file copied in different directories of the pod.
-        # we could calculate the md5sum by downloading first outside of pod and then comparing it with that of pod.
+        # (we could calculate the md5sum by downloading first outside of pod and then comparing it with that of pod.
         # But this will increase the execution time as we have to wait for download to complete once outside pod and
-        # once inside pod
-
+        # once inside pod)
         md5sum_val_expected = "016c37aa72f12e88127239467ff4962b"
+
+        # We are not using the pod.verify_data_integrity with the fedora dc pods as of now for the following reason:
+        # verify_data_integrity function in pod.py calls check_file_existence which in turn uses 'find' utility to see
+        # if the file given exists or not. In fedora pods which this function mainly deals with, the 'find' utility
+        # doesn't come by default. It has to be installed. While doing so, 'yum install findutils' hangs.
+        # the link "https://forums.fedoraforum.org/showthread.php?320926-failovemethod-option" mentions the solution:
+        # to run "sed -i '/^failovermethod=/d' /etc/yum.repos.d/*.repo". This command takes at least 6-7 minutes to
+        # complete. If this has to be repeated on 24 pods, then time taken to complete may vary between 20-30 minutes
+        # even if they are run in parallel threads.
+        # Instead of this if we use shell command: "md5sun" directly we can reduce the time drastically. And hence we
+        # are not using verify_data_integrity() here.
 
         for i in range(1, 10):
 
-            # cmd = "md5sum /mnt/copy_dir" + str(i) + "/ceph_" + str(i) + ".tar.gz"
-
-            cmd = "" + "md5sum /mnt/copy_dir" + str(i) + "/ceph_" + str(i) + ".tar.gz" + ""
-
-            output = pod.exec_sh_cmd_on_pod(cmd, sh="bash", timeout=None)
+            cmd = "" + "md5sum /mnt/"+ dir_name + "/copy_dir" + str(i) +"/ceph_" + str(i) + ".tar.gz " + ""
+            output = copy_pod.exec_sh_cmd_on_pod(cmd, sh="bash")
             md5sum_val_got = output.split("  ")[0]
-            logger.info(f"#### md5sum obtained for pod: {pod.name} is {md5sum_val_got}")
+            logger.info(f"#### md5sum obtained for pod: {copy_pod.name} is {md5sum_val_got}")
             logger.info(f"#### Expected was: {md5sum_val_expected}")
-            if md5sum_val_got not in md5sum_val_expected:
-                logging.error(f"***** md5sum check FAILED. expected: {md5sum_val_expected}, but got {md5sum_val_got}")
-                return False
-            else:
-                logging.info(f"#### md5sum check PASSED")
+            assert md5sum_val_got == md5sum_val_expected, \
+                f"***** md5sum check FAILED. expected: {md5sum_val_expected}, but got {md5sum_val_got}"
+
+        logging.info("#### Data Integrity check passed")
 
         # Remove the directories and loop back again
-        cmd = """ rm -rf /mnt/copy_dir{1..10} """
-        pod.exec_sh_cmd_on_pod(cmd, sh="bash", timeout=None)
+        cmd = "" + "rm -rf /mnt/" + dir_name + "/copy_dir{1..10}" + ""
+        logging.info(f"#### command to remove = {cmd}")
+        copy_pod.exec_sh_cmd_on_pod(cmd, sh="bash") #, timeout=None)
         logging.info(f"########### cluster_copy_ops: Completed {itr+1} Iterations")
-
-    return True
