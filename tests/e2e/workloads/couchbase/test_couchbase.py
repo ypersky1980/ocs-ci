b81f7a9f4f6264559daea1fdd62d152a2951dd76
Shreekar <sshreeka@redhat.com>
2020-05-27 11:08:07 +0530

    tests/e2e/workloads/couchbase/test_couchbase.py   - Test script for testing Couchbase workload with replicas
    tests/e2e/workloads/test_couchbase.py
     - Moved to tests/e2e/workloads/couchbase/test_couchbase.py
    
    Signed-off-by: Shreekar <sshreeka@redhat.com>

---
+++ tests/e2e/workloads/couchbase/test_couchbase.py
@@ -0,0 +1,31 @@
+import logging
+import pytest
+
+from ocs_ci.framework.testlib import E2ETest, workloads, ignore_leftovers
+from ocs_ci.ocs.couchbase import CouchBase
+
+log = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope='function')
+def couchbase(request):
+
+    couchbase = CouchBase()
+
+    def teardown():
+        couchbase.teardown()
+    request.addfinalizer(teardown)
+    return couchbase
+
+
+@workloads
+@ignore_leftovers
+@pytest.mark.polarion_id("OCS-807")
+class TestCouchBaseWorkload(E2ETest):
+    """
+    Deploy an CouchBase workload using operator
+    """
+    def test_cb_workload_simple(self, couchbase):
+        couchbase.create_couchbase_worker(replicas=5)
+        couchbase.run_workload(replicas=5)
+        couchbase.analyze_run()
